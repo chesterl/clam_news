2c6115f0970f39024c1dd233a46c799fd53eb692
Chester Lam <chesterlam620@gmail.com>
2015-04-10 20:28:55 -0700

    Fixed respond_to in comments controller to respond to different formats and redirect after comment post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,33 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+  # respond_to :html
 
   def create
     @link = Link.find(params[:link_id])
     @comment = @link.comments.new(comment_params)
     @comment.user = current_user
-    @comment.save
-    respond_with(@comment)
-  end
 
-  def update
-    @comment.update(comment_params)
-    respond_with(@comment)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @link, notice: "Comment was successfully created." }
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @comment.errors, status: unprocessable_entity }
+      end
+    end
+    # respond_with(@comment)
   end
 
   def destroy
     @comment.destroy
-    respond_with(@comment)
+
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+    # respond_with(@comment)
   end
 
   private
